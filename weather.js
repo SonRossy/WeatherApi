1d04c6388a002b7f8dfbd3ed2b06daa9b9c4e9c3
SonRossy <tristesse0889@hotmail.com>
2018-10-16 16:02:36 -0400

    initial commit

---
+++ weather.js
@@ -0,0 +1,36 @@
+$( document ).ready(() => {
+    let zipcode = $("#zipcode");
+    let showButton= $("#show-weather");
+
+
+    function sendReq(){
+        console.log(zipcode);
+        $.ajax({
+            url:"https://api.openweathermap.org/data/2.5/weather?zip="+zipcode.val()+",us&units=imperial&appid=c2f8212a26a7a84d3ab42886d3861a88",
+            type:"get",
+            success:(data)=>{
+                console.log(data);
+                $("#city-name").text(data.name);
+                $("#lat").text(data.coord.lat);
+                $("#lon").text(data.coord.lon);
+                $("#weather-main").text(data.weather[0].main);
+                $("#weather-desc").text(data.weather[0].description);
+                $("#wind-speed").text(data.wind.speed);
+                $("#wind-degree").text(data.wind.gust);
+                $("#temp-current").text(data.main.temp);
+                $("#temp-min").text(data.main.temp_min);
+                $("#temp-max").text(data.main.temp_max);
+                $("#humidity").text(data.main.humidity);
+            },
+            error:(error)=>{
+                alert(error);
+            }
+        });
+    }
+
+    showButton.on('click',()=> {
+        sendReq();
+    })
+});
+
+
